417c1d11cac279c22bde5b6ec6004e615f04e8a2
silvercobraa <silver.cobra1n@gmail.com>
2016-06-02 16:15:26 -0400

    Se agregó la descripción del método get_entrada.

--- TablaPagina.h
+++ TablaPagina.h
@@ -1,5 +1,5 @@
-#ifndef _TablaPagina_ 
-#define _TablaPagina_ 
+#ifndef _TablaPagina_
+#define _TablaPagina_
 
 #include <cstdio>
 #include <cstdlib>
@@ -23,22 +23,24 @@ class TablaPagina : public Tabla
   int tamano;
   //arreglo de entradas
   EntradaTP *entrada;
-  //contructor que recibe la cantidad de entradas de la tabla de pagina 
+  //contructor que recibe la cantidad de entradas de la tabla de pagina
   TablaPagina(int cantidad);
   //construcctor dado por defecto
   TablaPagina();
   int LRU(int direccion_virtual);
   EntradaTP* at(int posicion);
   void buscar_npv(unsigned int direccion_virtual);
+
+  // Retorna el objeto entrada que se encuentra en la posición 'posicion' de esta tabla de pagina
   EntradaTP get_entrada(int posicion);
 
-//se refiere al caso  V=1 y R=1 
+//se refiere al caso  V=1 y R=1
   bool primer_caso(int npv);
   //se refiere al caso V=0
   bool segundo_caso(int npv);
 //funcion que buscar la posicion de un marco libre, si no se encontre
   //retorna un 0
- 
+
 
 
 };
